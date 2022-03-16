a684e2f17058233cdb5b204fcc4ba2bf1eeddbbb
Roman_S <ramones.31rus@gmail.com>
2022-03-16 11:39:56 +0300

    update_templates

--- noc/gpon/views.py
+++ noc/gpon/views.py
@@ -87,6 +87,11 @@ class RequestCreate(LoginRequiredMixin, CreateView):
     template_name = 'gpon/request_form_create.html'
     form_class = RequestFormCreate
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['address'] = House.objects.get(pk=self.kwargs.get('pk'))
+        return context
+
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
